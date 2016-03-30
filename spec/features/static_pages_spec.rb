9e3addb72269514d87d8d3a9153390b249de0f13
nachocruel <edvansouza.es@gmail.com>
2016-03-30 20:42:52 -0300

    Finish layout and routes

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,73 +2,37 @@ require 'spec_helper'
 
 
 feature "Static pages" do
+ 
+ subject { page }
 
- let(:base_title) { "Ruby on Rails Tutorial Sample App" } 
- 
- 
-  feature "Home page" do
-    it "should have the h1 'Sample App'" do
-	    visit '/static_pages/home'
-	    page.should have_selector('h1', :text => 'Sample App')
-    end
+ feature "Home page" do
+
+    before { visit root_path }
 	
-	def failure_message
-          "expected there to be title #{title.inspect} in #{@actual.title.inspect}"
-    end
-	
-	def failure_message_when_negated
-          "expected there not to be title #{title.inspect} in #{@actual.title.inspect}"
-    end
-	
+    it { should have_selector('h1', text: 'Sample App') }
+    #it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
  end
  
  feature "Help page" do
-    it "should have the content 'Help'" do
-        visit '/static_pages/help'
-        page.should have_selector('h1', :text => 'Help')
-    end
-   
-    def failure_message
-          "expected there to be title #{title.inspect} in #{@actual.title.inspect}"
-    end
-	
-	def failure_message_when_negated
-          "expected there not to be title #{title.inspect} in #{@actual.title.inspect}"
-    end
+    before { visit help_path }
+    it { should have_selector('h1', text: 'Help') }
+    #it { should have_selector('title', text: full_title('Help')) }
    
  end
  
  feature "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
-	
-	def failure_message
-          "expected there to be title #{title.inspect} in #{@actual.title.inspect}"
-    end
-	
-	def failure_message_when_negated
-          "expected there not to be title #{title.inspect} in #{@actual.title.inspect}"
-    end
+    before { visit about_path }
+    it { should have_selector('h1', text: 'About') }
+    #it { should have_selector('title', text: full_title('About Us')) }
  end
  
  feature "Contact page" do
 
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
-    end
-	
-def failure_message
-          "expected there to be title #{title.inspect} in #{@actual.title.inspect}"
-    end
-	
-	def failure_message_when_negated
-          "expected there not to be title #{title.inspect} in #{@actual.title.inspect}"
-    end
-	
+    before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact') }
+    #it { should have_selector('title', text: full_title('Contact')) }
  end
  
 end
